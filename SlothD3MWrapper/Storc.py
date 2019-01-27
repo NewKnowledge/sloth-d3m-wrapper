bbfdbb9f20a9dc7c3cdcba0411a9238c84a5b97a
Jeffrey Gleason <jeffrey.gleason.3@gmail.com>
2019-01-27 14:43:25 -0600

    update python path according to new namespace

--- SlothD3MWrapper/Storc.py
+++ SlothD3MWrapper/Storc.py
@@ -75,7 +75,7 @@ class Storc(TransformerPrimitiveBase[Inputs, Outputs, Hyperparams]):
             ),
         }],
         # The same path the primitive is registered with entry points in setup.py.
-        'python_path': 'd3m.primitives.time_series_segmentation.time_series_clustering.Sloth.cluster',
+        'python_path': 'd3m.primitives.time_series_segmentation.time_series_clustering.Sloth',
         # Choose these from a controlled vocabulary in the schema. If anything is missing which would
         # best describe the primitive, make a merge request.
         'algorithm_types': [
