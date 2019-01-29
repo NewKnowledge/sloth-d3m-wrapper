e2a7eae6e98f4fc1b4792714cf145c05e9df3987
Ubuntu <jgleason@JeffreyVMDev.umi1swgzx55e3jlvq0v5igys1h.jx.internal.cloudapp.net>
2019-01-29 02:06:01 +0000

    update call to dataset_to_dataframe

--- SlothD3MWrapper/Storc.py
+++ SlothD3MWrapper/Storc.py
@@ -14,7 +14,7 @@ from d3m.container import DataFrame as d3m_DataFrame
 from d3m.metadata import hyperparams, base as metadata_base
 from common_primitives import utils as utils_cp, dataset_to_dataframe as DatasetToDataFrame
 
-#from timeseriesloader.timeseries_loader import TimeSeriesLoaderPrimitive
+from timeseriesloader.timeseries_loader import TimeSeriesLoaderPrimitive
 
 __author__ = 'Distil'
 __version__ = '2.0.1'
@@ -158,10 +158,10 @@ class Storc(TransformerPrimitiveBase[Inputs, Outputs, Hyperparams]):
         return CallResult(sloth_df)
 
 if __name__ == '__main__':
-    '''
+    
     # Load data and preprocessing
     input_dataset = container.Dataset.load('file:///data/home/jgleason/D3m/datasets/seed_datasets_current/66_chlorineConcentration/66_chlorineConcentration_dataset/datasetDoc.json')
-    ds2df_client = DatasetToDataFrame(hyperparams = {"dataframe_resource":"1"})
+    ds2df_client = DatasetToDataFrame.DatasetToDataFramePrimitive(hyperparams = {"dataframe_resource":"1"})
     df = d3m_DataFrame(ds2df_client.produce(inputs = input_dataset).value)    
     ts_loader = TimeSeriesLoaderPrimitive(hyperparams = {"time_col_index":0, "value_col_index":1,"file_col_index":1})
     metadata_dict = dict(df.metadata.query_column(ts_loader.hyperparams['file_col_index']))
@@ -176,4 +176,4 @@ if __name__ == '__main__':
     #frame = pandas.read_csv("path/csv_containing_one_series_per_row.csv",dtype=str)
     result = storc_client.produce(inputs = ts_values.value.head(100))
     print(result.value)
-    '''
+    
