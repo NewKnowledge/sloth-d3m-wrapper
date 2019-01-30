cee326736f1b5887a19108d296230220db10f8f1
Jeffrey Gleason <jeffrey.gleason.3@gmail.com>
2019-01-29 18:36:54 -0600

    bump version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='SlothD3MWrapper',
-    version='2.0.1',
+    version='2.0.2',
     description='A thin wrapper for interacting with New Knowledge time series tool library Sloth',
     packages=['SlothD3MWrapper'],
     install_requires=["Sloth==2.0.2"],
