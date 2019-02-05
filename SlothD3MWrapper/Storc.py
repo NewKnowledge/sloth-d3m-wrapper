d77cc720f0737b4c913568ea1697d4db6c03437f
Jeffrey Gleason <jeffrey.gleason.3@gmail.com>
2019-02-05 15:16:01 -0600

    add jg/editDeps dependency

--- SlothD3MWrapper/Storc.py
+++ SlothD3MWrapper/Storc.py
@@ -72,7 +72,7 @@ class Storc(TransformerPrimitiveBase[Inputs, Outputs, Hyperparams]):
             },
             {
                 "type": "PIP",
-                "package_uri": "git+https://github.com/NewKnowledge/sloth.git@816fa8cabefe317d29f52adfe69da71980a16a76#egg=Sloth"
+                "package_uri": "git+https://github.com/NewKnowledge/sloth.git@jg/editDeps#egg=Sloth"
             },
             {
             'type': metadata_base.PrimitiveInstallationType.PIP,
